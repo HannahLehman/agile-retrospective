ee277b44e00b92157d1ab4394d3553d107716d40
Hannah Lehman <hannah.lehman@dudesolutions.com>
2018-01-30 14:59:39 -0500

    Getting modules in place for testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { FirebaseApp } from "angularfire2";
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [ RouterTestingModule ],
+      providers: [
+        AngularFireAuth,
+        FirebaseApp
+      ]
     }).compileComponents();
   }));
 
@@ -17,10 +25,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'RetroMatic'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('RetroMatic');
   }));
 
   it('should render title in a h1 tag', async(() => {
